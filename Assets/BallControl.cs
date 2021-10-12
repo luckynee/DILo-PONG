ccf8071de38694bcf8eddcc0f72d09e8e6fd4b05
luckynee <janspurba64@gmail.com>
2021-10-12 08:48:06 +0700

    Project PONG

---
+++ Assets/BallControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    public float xInitialForce;
+    public float yInitialForce;
+    private Vector2 trajectoryOrigin;
+
+    void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        rigidbody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        //float yRandomInitialForce = Random.Range(-yInitialForce,yInitialForce);
+        float randomDirection = Random.Range(0, 2);
+        if(randomDirection < 1.0f)
+        {
+            rigidbody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
+        }
+        else
+        {
+            rigidbody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
+        }
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("PushBall", 2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        RestartGame();
+        trajectoryOrigin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+    // Untuk mengakses informasi titik asal lintasan
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
